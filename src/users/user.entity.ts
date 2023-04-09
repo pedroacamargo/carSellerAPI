3a585d4127177cab68aec8b1780bc10b4489fd39
pedroacamargo <pedroaugennes@gmail.com>
2023-04-09 21:33:39 +0100

    First commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,29 @@
+import { AfterInsert, BeforeRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+// All these things are decorators
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log("Inserted User with id ", this.id)
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log("Updated User with id ", this.id)
+  }
+
+  @BeforeRemove()
+  logRemove() {
+    console.log("Removed user with id ", this.id)
+  }
+}
